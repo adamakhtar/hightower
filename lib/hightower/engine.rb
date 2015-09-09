99f601c6fd310fc560044dd4142f07802c67a0b1
Adam Akhtar <adam.akhtar@gmail.com>
2015-09-09 23:35:13 +0900

    setup testing gems

--- lib/hightower/engine.rb
+++ lib/hightower/engine.rb
@@ -1,5 +1,10 @@
 module Hightower
   class Engine < ::Rails::Engine
     isolate_namespace Hightower
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
