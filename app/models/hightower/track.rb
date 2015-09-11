23ef297533f4c78832d36f62be928ec4c32cf659
Adam Akhtar <adam.akhtar@gmail.com>
2015-09-12 00:50:49 +0900

    create Track operation
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

---
+++ app/models/hightower/track.rb
@@ -0,0 +1,26 @@
+module Hightower 
+  class Track
+    def initialize(behaviour)
+      @behaviour = behaviour
+    end
+
+    def self.perform(behaviour)
+      new(behaviour).perform
+    end
+
+    def perform
+      segment = find_or_create_segment(behaviour)
+      users = behaviour.observe
+      segment.users << users
+      users
+    end
+
+    private
+
+    attr_reader :behaviour
+
+    def find_or_create_segment(behaviour)
+      Segment.where(name: behaviour.name).first_or_create
+    end
+  end
+end
---
+++ spec/models/hightower/track_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+module Hightower
+  RSpec.describe Track, type: :model do
+    describe "#perform" do
+      it "creates segment for behaviour if it doesnt exist" do
+        user = create(:user)
+        behaviour = stub_behaviour( name: 'slipping away users', users: [user])
+        
+        Track.perform(behaviour)
+
+        expect(Segment.where(name: 'slipping away users').count).to eq 1
+      end
+
+      it "adds users who match given behaviour to the segment" do
+        user = create(:user)
+        segment = create(:segment, name: 'slipping away users')
+        behaviour = stub_behaviour( name: 'slipping away users', users: [user])
+        
+        Track.perform(behaviour)
+
+        expect(segment.users).to eq [user]
+        expect(segment.personas.where(user: user, processed: false).count).to eq 1
+      end
+
+      def stub_behaviour(name:, users:)
+        behaviour = double(:behaviour, name: name, observe: users)
+      end
+    end
+  end
+end
